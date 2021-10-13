2940e92bda91170bd469dd9a4e1b3e4989ca7785
Haf <haf@protonmail.ch>
2021-10-13 11:38:44 +0200

    testing NDC

--- src/test/java/xyz/haff/aspektoj/aspects/TraceTest.java
+++ src/test/java/xyz/haff/aspektoj/aspects/TraceTest.java
@@ -2,6 +2,7 @@ package xyz.haff.aspektoj.aspects;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import xyz.haff.aspektoj.annotations.Traced;
@@ -23,22 +24,25 @@ class TraceTest {
         innerTracedMethod();
     }
 
-    // TODO: Test NDC?
-
     @Test
     void testTraced() {
         try (MockedStatic<LogManager> logManager = mockStatic(LogManager.class)) {
-            var mockLogger = mock(Logger.class);
-            logManager.when(() -> LogManager.getLogger(anyString())).thenReturn(mockLogger);
-            tracedMethod();
+            try (MockedStatic<ThreadContext> threadContext = mockStatic(ThreadContext.class)) {
+                var mockLogger = mock(Logger.class);
+                logManager.when(() -> LogManager.getLogger(anyString())).thenReturn(mockLogger);
+                tracedMethod();
 
-            var inOrder = inOrder(mockLogger);
+                var inOrder = inOrder(mockLogger);
 
-            inOrder.verify(mockLogger).trace(eq("Calling void xyz.haff.aspektoj.aspects.TraceTest.tracedMethod()..."));
-            inOrder.verify(mockLogger).trace(eq("Calling void xyz.haff.aspektoj.aspects.TraceTest.innerTracedMethod()..."));
-            inOrder.verify(mockLogger).trace(eq("Calling void java.io.PrintStream.println(String)..."));
-            inOrder.verify(mockLogger).trace(eq("Calling void xyz.haff.aspektoj.aspects.TraceTest.innerTracedMethod()..."));
-            inOrder.verify(mockLogger).trace(eq("Calling void java.io.PrintStream.println(String)..."));
+                inOrder.verify(mockLogger).trace(eq("Calling void xyz.haff.aspektoj.aspects.TraceTest.tracedMethod()..."));
+                inOrder.verify(mockLogger).trace(eq("Calling void xyz.haff.aspektoj.aspects.TraceTest.innerTracedMethod()..."));
+                inOrder.verify(mockLogger).trace(eq("Calling void java.io.PrintStream.println(String)..."));
+                inOrder.verify(mockLogger).trace(eq("Calling void xyz.haff.aspektoj.aspects.TraceTest.innerTracedMethod()..."));
+                inOrder.verify(mockLogger).trace(eq("Calling void java.io.PrintStream.println(String)..."));
+
+                threadContext.verify(() -> ThreadContext.push(anyString()), times(5));
+                threadContext.verify(ThreadContext::pop, times(5));
+            }
         }
     }
 }
